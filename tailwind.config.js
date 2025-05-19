91d644500a4cb93ae9b563f810352a16aadc228c
coolnsx <ansari.tan20@gmail.com>
2025-05-19 15:42:13 +0530

    feat: ui changes again

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  purge: ["./templates/**/*.html", "./theme/**/*.html"],
-  darkMode: "class",
-  theme: {},
-  variants: {},
-  plugins: [
-      require('@tailwindcss/typography'),
-  ],
-};
